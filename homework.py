d7931732a9c64df729def267d45a8c9985575038
Vladislav Guzov <tim.barr@yandex.ru>
2022-06-15 12:13:25 +0300

    Small fixes

--- homework.py
+++ homework.py
@@ -13,7 +13,7 @@ from exeptions import (
 from typing import Union
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(stream=sys.stdout)
 formatter = logging.Formatter(
     '%(asctime)s %(levelname)s %(message)s'
@@ -40,7 +40,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 30
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
